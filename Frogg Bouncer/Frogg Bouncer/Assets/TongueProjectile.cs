3dbc25797559f9618dc53d25807e7213d549bfd7
nkp-2001 <83334420+nkp-2001@users.noreply.github.com>
2024-05-05 14:34:36 +0200

    Tounge Bounce Bag and Visual Update

--- Frogg Bouncer/Frogg Bouncer/Assets/TongueProjectile.cs
+++ Frogg Bouncer/Frogg Bouncer/Assets/TongueProjectile.cs
@@ -5,32 +5,74 @@ using UnityEngine;
 public class TongueProjectile : MonoBehaviour
 {
     Rigidbody2D rigi;
-    // Start is called before the first frame update
+    [SerializeField] bool hit = false;
+    LineRenderer lineRenderer;
+    TrailRenderer trailRenderer;
+    Vector3 oringP;
+
     void Awake()
     {
-        rigi = GetComponent<Rigidbody2D>(); 
+        rigi = GetComponent<Rigidbody2D>();
+        lineRenderer = GetComponent<LineRenderer>();
+        trailRenderer = GetComponent<TrailRenderer>();
+    }
+    private void Update()
+    {
+        if (hit)
+        {
+            Vector3[] points = new[] { oringP, transform.position };
+            lineRenderer.SetPositions(points);
+            
+            if(Vector2.Distance(transform.position, oringP) < 0.01f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void Start()
+    {
+        oringP = transform.position;
     }
     public void Push(Vector2 dir,float strength)
     {
+        
         rigi.velocity = dir* strength;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-
-        EnemyType typeOfEnemiy = collision.gameObject.GetComponent<EnemyMovement>().GetEnemyType();
-
-        
-        print(typeOfEnemiy);
-        if (typeOfEnemiy == EnemyType.Fairy)
+        if (!hit)
         {
-            CodeEventHandler.Trigger_LosingLife();
+            if (collision.tag != "MissedTarget")
+            {
+
+
+                EnemyType typeOfEnemiy = collision.gameObject.GetComponent<EnemyMovement>().GetEnemyType();
+
+
+                print(typeOfEnemiy);
+                if (typeOfEnemiy == EnemyType.Fairy)
+                {
+                    CodeEventHandler.Trigger_LosingLife();
+                }
+                else
+                {
+
+                    CodeEventHandler.Trigger_GettingPointsRaw();
+                }
+                Destroy(collision.gameObject);
+            }
+
+            hit = true;
+            trailRenderer.widthMultiplier = 0.0f;
+
+            rigi.gravityScale = 0.0f;
+            Push(oringP - transform.position, 5);
+
+
         }
-        else
-        {
-          
-            CodeEventHandler.Trigger_GettingPointsRaw();
-        }
-        Destroy(collision.gameObject);
+
+
+
     }
 
 
