38589faa0e65b93d58f034bded32f818a31d5645
Densch <dennis.schaffner@gmx.de>
2024-05-13 20:46:38 +0200

    [ADDED] Pause Menu weiter angepasst, Audio Scripte erstellt

---
+++ Frogg Bouncer/Frogg Bouncer/Assets/Code/UI/VolumeChanger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+    public class VolumeChanger : MonoBehaviour
+    {
+        public AudioMixer audioMixer;
+
+        public void SetMainVolume(float volume)
+        {
+            audioMixer.SetFloat("MainVolume", volume);
+            Debug.Log("MainVolume");
+        }
+        public void SetMusicVolume(float volume)
+        {
+            audioMixer.SetFloat("MusicVolume", volume);
+        }
+        public void SetSFXVolume(float volume)
+        {
+            audioMixer.SetFloat("SFXVolume", volume);
+        }
+
+        public void MuteToggle(bool muted)
+        {
+            if (muted)
+            {
+                AudioListener.volume = 0;
+            }
+            else
+            {
+                AudioListener.volume = 1;
+            }
+
+        }
+    }
